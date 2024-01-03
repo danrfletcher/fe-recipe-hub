28061cb8ca87419632bda49c8b3588a737cf8881
gcpearse <glen.c.pearse@gmail.com>
2024-01-03 17:03:22 +0000

    adds navSlice reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
+import navReducer from "../features/navSlice"
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    toggle: navReducer
+  }
 })
 
 export type AppDispatch = typeof store.dispatch
