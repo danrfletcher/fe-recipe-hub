a5ed112842d89afabc61475dc2a42065ed12ec7d
gcpearse <glen.c.pearse@gmail.com>
2024-01-03 17:04:24 +0000

    adds nav toggle functionality to menu button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,30 @@
 import { SlMenu } from "react-icons/sl";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { toggle } from "../features/navSlice";
+import { ToggledNav } from "./Nav";
 
 const Header: React.FC = () => {
+
+  const isToggled = useAppSelector((state) => state.toggle.value)
+
+  const dispatch = useAppDispatch()
+
   return (
-    <header>
-      <h1>Umami</h1>
-      <div className="icon-container">
-        <SlMenu className="icon" />
-      </div>
-    </header>
+    <>
+      <header>
+        <h1>Umami</h1>
+        <div
+          className="icon-container"
+          onClick={() => dispatch(toggle())}>
+          <SlMenu className="icon" />
+        </div>
+      </header>
+      {isToggled ? (
+        <ToggledNav />
+      ) : (
+        null
+      )}
+    </>
   )
 }
 
