b291b46428bff77f4fcfe5be0f66e1f8f20291cf
gcpearse <glen.c.pearse@gmail.com>
2024-01-03 12:15:37 +0000

    styling update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import { SlMenu } from "react-icons/sl";
+
 const Header: React.FC = () => {
   return (
     <header>
       <h1>Umami</h1>
-      <div className="icon-container"></div>
+      <div className="icon-container">
+        <SlMenu style={{background: "transparent", color: "#660f27", cursor: "pointer"}} />
+      </div>
     </header>
   )
 }
