f904bcf922da2d42e38ca84d760b7d01135bd2e5
gcpearse <glen.c.pearse@gmail.com>
2024-01-04 14:51:47 +0000

    removes comments from initial setup

--- src/app/store.ts
+++ src/app/store.ts
@@ -11,14 +11,3 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
-
-/*
-Redux contains the entirety of the app's state in a JavaScript object called the store.
-
-The store has access to the entire App component because it is called by a provider (see main.tsx).
-
-In lines 3-5, we are making sure the store is aware of any reducers we have made in our slices.
-
-In lines 7-8, we are declaring and exporting the type of AppDispatch and RootState based on our store.
-These are then imported and used in hooks.ts when we add types to our hooks.
-*/
