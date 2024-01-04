dfe61ffe1b1d1b23479051492bcc53b3f2d28537
gcpearse <glen.c.pearse@gmail.com>
2024-01-04 00:03:33 +0000

    switches icon from burger menu to arrow when nav bar is toggled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,33 @@
-import { SlMenu } from "react-icons/sl";
+import { SlArrowUp, SlMenu } from "react-icons/sl";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { toggle } from "../features/navSlice";
 import { ToggledNav } from "./Nav";
 
 const Header: React.FC = () => {
-	const isToggled = useAppSelector((state) => state.toggle.value);
 
-	const dispatch = useAppDispatch();
+  const isToggled = useAppSelector((state) => state.toggle.value);
+  const dispatch = useAppDispatch();
 
-	return (
-		<>
-			<header>
-				<h1>Umami</h1>
-				<div className="icon-container" 
-        onClick={() => dispatch(toggle())}>
-					<SlMenu className="icon" />
-				</div>
-			</header>
-			{isToggled ? (
-				<ToggledNav class={"slide-in"} />
-			) : (
-				<ToggledNav class={"slide-out"} />
-			)}
-		</>
-	);
+  return (
+    <>
+      <header>
+        <h1>Umami</h1>
+        <div className="icon-container"
+          onClick={() => dispatch(toggle())}>
+          {!isToggled ? (
+            <SlMenu className="icon" />
+          ) : (
+            <SlArrowUp className="icon" />
+          )}
+        </div>
+      </header>
+      {isToggled ? (
+        <ToggledNav class={"slide-in"} />
+      ) : (
+        <ToggledNav class={"slide-out"} />
+      )}
+    </>
+  );
 };
 
 export default Header;
