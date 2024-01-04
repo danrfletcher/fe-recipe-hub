373bad32dfd7a00a4559c1354e9bd2bd3ceaa7b7
gcpearse <glen.c.pearse@gmail.com>
2024-01-04 14:38:16 +0000

    updates store with auth reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import navReducer from "../features/navSlice"
+import authReducer from "../features/auth/authSlice"
 
 export const store = configureStore({
   reducer: {
-    toggle: navReducer
+    toggle: navReducer,
+    auth: authReducer
   }
 })
 
