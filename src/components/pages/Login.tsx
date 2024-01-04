20d9d2a48b01ef9fba65cd47a1c8b6f506697f29
gcpearse <glen.c.pearse@gmail.com>
2024-01-04 14:05:36 +0000

    sets up basic login page using React Hook Form

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,7 +1,44 @@
+import { SubmitHandler, useForm } from "react-hook-form"
+import { Link } from "react-router-dom"
+
+interface FormValues {
+  username: string
+  password: string
+}
+
 const Login: React.FC = () => {
+
+  const { register, handleSubmit } = useForm<FormValues>()
+  const submitForm: SubmitHandler<FormValues> = (data) => {
+    console.log(data.username)
+  }
+
   return (
     <div className="page-container">
       <h2>Log in</h2>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            autoComplete="on"
+            {...register("username")}
+            required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            {...register("password")}
+            required />
+        </div>
+        <button type="submit" className="button">
+          Log in
+        </button>
+      </form>
+      <p>Not signed up? <Link to="/register">Register</Link></p>
     </div>
   )
 }
