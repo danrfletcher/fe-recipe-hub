bd2b885b956c9f5628bece0f33965bccbdbff5bd
gcpearse <glen.c.pearse@gmail.com>
2024-01-05 23:08:45 +0000

    displays current user's username on success page

--- src/components/pages/LoginSuccess.tsx
+++ src/components/pages/LoginSuccess.tsx
@@ -5,7 +5,8 @@ import { logout } from "../../features/auth/authSlice"
 const LoginSuccess: React.FC = () => {
 
   const isNavToggled = useAppSelector((state) => state.navToggle.value)
-  
+  const user = useAppSelector((state) => state.user.value)
+
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
 
@@ -17,6 +18,8 @@ const LoginSuccess: React.FC = () => {
   return (
     <div className={isNavToggled ? "page-slide-in" : "page-slide-out"}>
       <h2 className="login-header">Welcome back!</h2>
+      <p>You are currently logged in as</p>
+      <span>{user}</span>
       <button
         className="login-btn"
         onClick={handleClick}>
