15e877544298562c4f3e05b554074f18ac072c5d
Agata-M-S <agata.webdevelopment@gmail.com>
2024-01-05 21:06:39 +0000

    add user registration form and registration request to the api

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -5,10 +5,12 @@ import { api } from "../../utils/api-utils";
 interface AuthState {
 	isAuthenticated: boolean;
 	token: string | null;
+	hasRegistered: boolean;
 }
 const initialState: AuthState = {
 	isAuthenticated: false,
 	token: null,
+	hasRegistered: false,
 };
 
 const authSlice = createSlice({
@@ -29,15 +31,27 @@ const authSlice = createSlice({
 	},
 });
 
-export const login = (username: string, password: string): AppThunk =>
+export const login =
+	(username: string, password: string): AppThunk =>
 	async (dispatch) => {
 		try {
 			const response = await api.post("/login", { username, password });
 			const token = response.data;
 			dispatch(loginSuccess(token));
 		} catch (error: any) {
-			console.log(error.message)
+			console.log(error.message);
 			// console.error("Login error:", error.response.data);
+		}
+	};
+
+export const registerUser =
+	(username: string,name: string,ProfileImg: string, password: string, bio: string): AppThunk =>
+	async () => {
+		try {
+      await api.post("/register", { username, name, ProfileImg, password, bio  });
+			console.log("registration successful");
+		} catch (error: any) {
+			console.log(error);
 		}
 	};
 
