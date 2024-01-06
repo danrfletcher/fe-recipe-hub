10d64bba143e41ba88678d89d8063a365f47488f
Agata-M-S <agata.webdevelopment@gmail.com>
2024-01-06 21:15:46 +0000

    add api request for all recipes endpoint

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,21 +1,25 @@
-import { configureStore, ThunkAction, Action  } from "@reduxjs/toolkit"
-import navReducer from "../features/navSlice"
-import authReducer from "../features/auth/authSlice"
-import recipesReducer from "../features/allRecipiesSlice"
-
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import navReducer from "../features/navSlice";
+import authReducer from "../features/auth/authSlice";
+import recipesReducer from "../features/allRecipesSlice";
 
 export const store = configureStore({
-  reducer: {
-    navToggle: navReducer,
-    auth: authReducer,
-    recipes: recipesReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-})
+	reducer: {
+		navToggle: navReducer,
+		auth: authReducer,
+		recipes: recipesReducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
+});
 
-export type AppDispatch = typeof store.dispatch
-export type RootState = ReturnType<typeof store.getState>
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+	ReturnType,
+	RootState,
+	unknown,
+	Action<string>
+>;
--- src/components/pages/Recipes.tsx
+++ src/components/pages/Recipes.tsx
@@ -1,24 +1,21 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
-import { getAllRecipes } from "../../features/allRecipiesSlice";
+import { getAllRecipes } from "../../features/allRecipesSlice";
 
 const Recipes: React.FC = () => {
+	const isNavToggled = useAppSelector((state) => state.navToggle.value);
+	const listOfRecipes = useAppSelector((state) => state.recipes.allRecipes);
+	const dispatch = useAppDispatch();
 
-  const isNavToggled = useAppSelector((state) => state.navToggle.value);
-  const listOfRecipes = useAppSelector((state)=>state.recipes.allRecipes)
-  const dispatch = useAppDispatch()  
+	useEffect(() => {
+		dispatch(getAllRecipes());
+	}, []);
+console.log(listOfRecipes)
+	return (
+		<div className={isNavToggled ? "page-slide-in" : "page-slide-out"}>
+			<h2>Recipes</h2>
+		</div>
+	);
+};
 
-
-  useEffect(()=>{
-    dispatch(getAllRecipes())
-  },[])
-  
-  
-  return (
-    <div className={isNavToggled ? "page-slide-in" : "page-slide-out"}>
-      <h2>Recipes</h2>
-    </div>
-  )
-}
-
-export default Recipes
+export default Recipes;
