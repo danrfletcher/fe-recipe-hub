e3f7154e3ab3ac6925926bdae4864c007d7254fc
gcpearse <glen.c.pearse@gmail.com>
2024-01-06 19:33:52 +0000

    provides placeholder for message on Error component

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom"
 
-const Error: React.FC = () => {
+const Error: React.FC = (props: any) => {
 
   const navigate = useNavigate()
 
   return (
     <section>
       <h2>Error</h2>
+      <p>{props.message}</p>
       <button
         onClick={() => navigate(-1)}>
         Back
