4da4a4631f21e0afbc39dcf00d963b93f3173bf5
Agata-M-S <agata.webdevelopment@gmail.com>
2024-01-07 02:59:33 +0000

    add single recipie api and pages

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,13 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import navReducer from "../features/navSlice";
 import authReducer from "../features/auth/authSlice";
 import recipesReducer from "../features/allRecipesSlice";
-
+import singleRecipeReducer from "../features/singleRecipeSlice.ts"
 export const store = configureStore({
 	reducer: {
 		navToggle: navReducer,
 		auth: authReducer,
 		recipes: recipesReducer,
+    singleRecipe: singleRecipeReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({
---
+++ src/components/pages/SingleRecipe.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { getSingleRecipe } from "../../features/singleRecipeSlice";
+
+const SingleRecipe: React.FC = () => {
+  const recipeData = useAppSelector((state) => state.singleRecipe.recipe);
+  const recipeId = useAppSelector((state) => state.singleRecipe.recipeId);
+  const isLoading = useAppSelector(state => state.singleRecipe.isLoading)
+	const dispatch = useAppDispatch();
+  
+	console.log(recipeData);
+	useEffect(() => {
+    dispatch(getSingleRecipe(recipeId));
+	}, []);
+	return (
+		<div>
+			<h2>{recipeData.recipeTitle}</h2>
+			<p>{recipeData.tagLine}</p>
+			<img src={recipeData.recipeImg} />
+			<br />
+			<span>Prep time: {recipeData.timeToPrepare} mins </span>
+			<span>Difficulty: {recipeData.difficulty}</span>
+			<div>
+       Ingredients:
+     {
+     !isLoading ?    
+          recipeData.recipeIngredients.map((ingredient: any)=>{
+            return <li key={ingredient.ingredientId}>
+              {ingredient.ingredientName}
+            </li> 
+          }): null
+        }
+
+      </div>
+		</div>
+	);
+};
+
+export default SingleRecipe;
