130c32f362ab41879869ecc4855afd424739e22a
gcpearse <glen.c.pearse@gmail.com>
2024-01-07 21:17:16 +0000

    formatting and styling of recipe card, inc formatted date and difficulty setting

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -2,9 +2,11 @@ import { useNavigate } from "react-router-dom"
 import { useAppDispatch } from "../app/hooks";
 import { recipeId } from "../features/singleRecipeSlice";
 import { Recipe } from "../features/allRecipesSlice";
+import { lengthenDate } from "../utils/formatting-utils";
+import { LuChefHat } from "react-icons/lu";
 
 const RecipeCard: React.FC<Recipe> = (props) => {
-	
+
 	const dispatch = useAppDispatch()
 	const navigate = useNavigate()
 
@@ -13,12 +15,39 @@ const RecipeCard: React.FC<Recipe> = (props) => {
 		dispatch(recipeId(props.recipeId))
 	}
 
+	const setDifficulty = (rating: number) => {
+		const array = []
+		for (let i = 0; i < rating; i++) {
+			array.push(<LuChefHat className="difficulty-icon" key={i} />)
+		}
+		return array
+	}
+
 	return (
-		<div onClick={handleClick}>
-			<img src={props.recipeImg} />
-			<h3>{props.recipeTitle}</h3>
-			<p>{props.tagLine}</p>
-			<span>Difficulty: {props.difficulty}</span><br /><span>preparation time: {props.timeToPrepare} mins</span>
+		<div className="recipe-wrapper">
+			<img
+				className="recipe-img"
+				src={props.recipeImg}
+				onClick={handleClick} />
+			<div className="recipe-info-wrapper">
+				<p className="recipe-el timestamp">
+					{lengthenDate(props.postedOn)}
+				</p>
+				<h3 className="recipe-el recipe-title">
+					{props.recipeTitle}
+				</h3>
+				<p className="recipe-el">
+					{props.tagLine}
+				</p>
+				<div className="recipe-facts-wrapper">
+					<p className="recipe-facts recipe-el">
+						Difficulty: {setDifficulty(props.difficulty)}
+					</p>
+					<p className="recipe-facts recipe-el">
+						Prep time: {props.timeToPrepare} minutes
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
