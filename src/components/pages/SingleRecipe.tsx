f3f965becffe299b71c6618e8330d39393e732f1
Agata-M-S <agata.webdevelopment@gmail.com>
2024-01-07 15:55:56 +0000

    add method display to the single recipe component

--- src/components/pages/SingleRecipe.tsx
+++ src/components/pages/SingleRecipe.tsx
@@ -3,14 +3,14 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { getSingleRecipe } from "../../features/singleRecipeSlice";
 
 const SingleRecipe: React.FC = () => {
-  const recipeData = useAppSelector((state) => state.singleRecipe.recipe);
-  const recipeId = useAppSelector((state) => state.singleRecipe.recipeId);
-  const isLoading = useAppSelector(state => state.singleRecipe.isLoading)
+	const recipeData = useAppSelector((state) => state.singleRecipe.recipe);
+	const recipeId = useAppSelector((state) => state.singleRecipe.recipeId);
+	const isLoading = useAppSelector((state) => state.singleRecipe.isLoading);
 	const dispatch = useAppDispatch();
-  
+
 	console.log(recipeData);
 	useEffect(() => {
-    dispatch(getSingleRecipe(recipeId));
+		dispatch(getSingleRecipe(recipeId));
 	}, []);
 	return (
 		<div>
@@ -21,17 +21,21 @@ const SingleRecipe: React.FC = () => {
 			<span>Prep time: {recipeData.timeToPrepare} mins </span>
 			<span>Difficulty: {recipeData.difficulty}</span>
 			<div>
-       Ingredients:
-     {
-     !isLoading ?    
-          recipeData.recipeIngredients.map((ingredient: any)=>{
-            return <li key={ingredient.ingredientId}>
-              {ingredient.ingredientName}
-            </li> 
-          }): null
-        }
-
-      </div>
+				Ingredients:
+				{!isLoading
+					? recipeData.recipeIngredients.map((ingredient: any) => {
+							return (
+								<li key={ingredient.ingredientId}>
+									{ingredient.ingredientName}
+								</li>
+							);
+					  })
+					: null}
+			</div>
+			<div>
+				Method:
+				<p>{recipeData.recipeMethod}</p>
+			</div>
 		</div>
 	);
 };
