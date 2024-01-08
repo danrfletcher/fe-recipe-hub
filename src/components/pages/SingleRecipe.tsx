5fae072dec6af17f9c04937226044deaacea5c72
gcpearse <glen.c.pearse@gmail.com>
2024-01-08 02:56:38 +0000

    uses useParams to get the recipeID from the URL and pass it to getSingleRecipe

--- src/components/pages/SingleRecipe.tsx
+++ src/components/pages/SingleRecipe.tsx
@@ -2,11 +2,14 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { getSingleRecipe } from "../../features/singleRecipeSlice";
 import Loading from "../Loading";
+import { Ingredient } from "../../features/ingredientsSlice";
+import { useParams } from "react-router-dom";
 
 const SingleRecipe: React.FC = () => {
 
 	const recipeData = useAppSelector((state) => state.singleRecipe.recipe);
-	const recipeId = useAppSelector((state) => state.singleRecipe.recipeId);
+	// Without useParams, map() in line 35 will not work - useParams takes the recipeId from the URL and fixes this issue
+	const { recipeId } = useParams()
 	const isLoading = useAppSelector((state) => state.singleRecipe.isLoading);
 
 	const dispatch = useAppDispatch();
@@ -29,7 +32,7 @@ const SingleRecipe: React.FC = () => {
 					<span>Difficulty: {recipeData.difficulty}</span>
 					<h3>Ingredients</h3>
 					<ul>
-						{recipeData.recipeIngredients.map((ingredient: any) => {
+						{recipeData.recipeIngredients.map((ingredient: Ingredient) => {
 							return (
 								<li key={ingredient.ingredientId}>
 									{ingredient.ingredientName}
