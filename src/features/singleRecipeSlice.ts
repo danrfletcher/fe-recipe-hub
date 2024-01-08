585ce79597e3558b907b6198a84b3973f7eb8a88
gcpearse <glen.c.pearse@gmail.com>
2024-01-08 02:56:59 +0000

    removes instances of type any and replaces them with custom types and interfaces

--- src/features/singleRecipeSlice.ts
+++ src/features/singleRecipeSlice.ts
@@ -1,15 +1,35 @@
 import { AppThunk } from "../app/store"
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { api } from "../utils/api-utils";
+import { Recipe } from "./allRecipesSlice";
+
+interface SingleRecipe extends Recipe {
+  recipeIngredients: []
+}
 
 interface SingleRecipeState {
-  recipe: any,
+  recipe: SingleRecipe,
   recipeId: string,
   isLoading: boolean
 }
 
 const initialState: SingleRecipeState = {
-  recipe: [],
+  recipe: {
+    recipeId: "",
+    recipeTitle: "",
+    tagLine: "",
+    difficulty: 0,
+    timeToPrepare: 0,
+    recipeMethod: "",
+    postedOn: "",
+    recipeImg: "",
+    cuisine: "",
+    forkedFromId: 0,
+    originalRecipeId: 0,
+    userId: 0,
+    cuisineId: 0,
+    recipeIngredients: []
+  },
   recipeId: "",
   isLoading: false
 };
@@ -18,7 +38,7 @@ const singleRecipeSlice = createSlice({
   name: "singleRecipe",
   initialState,
   reducers: {
-    singleRecipe: (state, action: PayloadAction<any>) => {
+    singleRecipe: (state, action: PayloadAction<SingleRecipe>) => {
       state.recipe = action.payload
       state.isLoading = false
       state.recipeId = action.payload.recipeId
@@ -30,7 +50,7 @@ const singleRecipeSlice = createSlice({
   }
 })
 
-export const getSingleRecipe = (recipeId: string): AppThunk => {
+export const getSingleRecipe = (recipeId: string | undefined): AppThunk => {
   return async (dispatch) => {
     try {
       const response = await api.get(`/recipes/${recipeId}`);
