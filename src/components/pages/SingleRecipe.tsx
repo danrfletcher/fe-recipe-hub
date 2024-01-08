d4889cb67b27aaf024b4b8ec2f2ec458356f93b8
gcpearse <glen.c.pearse@gmail.com>
2024-01-08 02:07:47 +0000

    work on loading screen

--- src/components/pages/SingleRecipe.tsx
+++ src/components/pages/SingleRecipe.tsx
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { getSingleRecipe } from "../../features/singleRecipeSlice";
+import Loading from "../Loading";
 
 const SingleRecipe: React.FC = () => {
 
 	const recipeData = useAppSelector((state) => state.singleRecipe.recipe);
 	const recipeId = useAppSelector((state) => state.singleRecipe.recipeId);
 	const isLoading = useAppSelector((state) => state.singleRecipe.isLoading);
-	
+
 	const dispatch = useAppDispatch();
 
 	useEffect(() => {
@@ -16,30 +17,32 @@ const SingleRecipe: React.FC = () => {
 
 	return (
 		<div>
-			<h2>{recipeData.recipeTitle}</h2>
-			<p>{recipeData.tagLine}</p>
-			<img src={recipeData.recipeImg} className="single-recipe-img" />
-			<br />
-			<span>Prep time: {recipeData.timeToPrepare} mins </span>
-			<span>Difficulty: {recipeData.difficulty}</span>
-			<div>
-				Ingredients:
-				{!isLoading ? (
-					recipeData.recipeIngredients.map((ingredient: any) => {
-						return (
-							<li key={ingredient.ingredientId}>
-								{ingredient.ingredientName}
-							</li>
-						);
-					})
-				) : (
-					null
-				)}
-			</div>
-			<div>
-				Method:
-				<p>{recipeData.recipeMethod}</p>
-			</div>
+			{isLoading ? (
+				<Loading />
+			) : (
+				<>
+					<h2>{recipeData.recipeTitle}</h2>
+					<p>{recipeData.tagLine}</p>
+					<img src={recipeData.recipeImg} className="single-recipe-img" />
+					<br />
+					<span>Prep time: {recipeData.timeToPrepare} mins </span>
+					<span>Difficulty: {recipeData.difficulty}</span>
+					<h3>Ingredients</h3>
+					<ul>
+						{recipeData.recipeIngredients.map((ingredient: any) => {
+							return (
+								<li key={ingredient.ingredientId}>
+									{ingredient.ingredientName}
+								</li>
+							)
+						})}
+					</ul>
+					<div>
+						<h3>Method</h3>
+						<p>{recipeData.recipeMethod}</p>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
