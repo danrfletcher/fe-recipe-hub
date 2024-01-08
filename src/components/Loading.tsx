e7883eff01fb59db8503dacc7459e11d0705c94a
gcpearse <glen.c.pearse@gmail.com>
2024-01-08 02:07:56 +0000

    adds loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import { ScaleLoader } from "react-spinners"
+
+const Loading: React.FC = () => {
+  return (
+    <>
+      <h2>Loading content...</h2>
+      <ScaleLoader />
+    </>
+  )
+}
+
+export default Loading
