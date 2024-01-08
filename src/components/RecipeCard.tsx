0e1008957020cf47c8b9b05aa70406b0a735611a
gcpearse <glen.c.pearse@gmail.com>
2024-01-08 00:37:56 +0000

    updates setDifficulty function to show max difficulty with empty icons

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -20,6 +20,9 @@ const RecipeCard: React.FC<Recipe> = (props) => {
 		for (let i = 0; i < rating; i++) {
 			array.push(<LuChefHat className="difficulty-icon" key={i} />)
 		}
+		for (let i = rating; i < 5; i++) {
+			array.push(<LuChefHat key={i} />)
+		}
 		return array
 	}
 
