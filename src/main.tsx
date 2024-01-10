530f02ca1023464f7032030b7bc6346e6fb8eab0
Dan <57096365+danrfletcher@users.noreply.github.com>
2024-01-10 17:58:16 +0000

    Add react bootstrap stylesheet to main

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
