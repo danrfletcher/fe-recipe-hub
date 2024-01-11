39252ec83c69752f1f07c66a859114b9359e5626
gcpearse <glen.c.pearse@gmail.com>
2024-01-11 11:45:34 +0000

    styling for FileUpload component

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -7,7 +7,7 @@ import { getUrl } from "../features/fileUploadSlice";
 // this component handles picture upload
 export const FileUpload: React.FC = () => {
 	const { register, handleSubmit,
-  formState: { isDirty }  } = useForm({ mode: "onChange" })
+		formState: { isDirty } } = useForm({ mode: "onChange" })
 
 	const dispatch = useAppDispatch();
 
@@ -15,16 +15,22 @@ export const FileUpload: React.FC = () => {
 		const formData = new FormData();
 		formData.append("file", data.file[0]);
 		dispatch(getUrl(formData));
-  
 	};
 
 	return (
-    <form onSubmit={handleSubmit(submitForm)} className="auth-form">
-          <label htmlFor="filename" className="input-label add-img-label">Add your image</label>
-        <div className="add-img-div input-field">
-  			<input className="add-img" type="file" id="filename" {...register("file")}/>
-  			<button type="submit" disabled={ !isDirty} className="styled-btn back-btn add-img-btn" >Add</button>
-		</div>
-  		</form>
-  	);
+		<form onSubmit={handleSubmit(submitForm)} className="auth-form">
+			<label htmlFor="filename" className="input-label add-img-label">
+				Upload Image
+			</label>
+			<div className="add-img-div">
+				<input type="file" id="filename" {...register("file")} />
+				<button
+					type="submit"
+					className="add-img-btn"
+					disabled={!isDirty}>
+					Add
+				</button>
+			</div>
+		</form>
+	);
 };
