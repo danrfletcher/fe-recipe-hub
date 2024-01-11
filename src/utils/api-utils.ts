8e2dc091f2eb698b90aa4a833aa654039f9717a1
gcpearse <glen.c.pearse@gmail.com>
2024-01-11 13:03:31 +0000

    IP change

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from "axios"
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "http://ec2-13-41-65-163.eu-west-2.compute.amazonaws.com:5000/api"
+  baseURL: "http://ec2-13-41-225-59.eu-west-2.compute.amazonaws.com:5000/api"
 })
