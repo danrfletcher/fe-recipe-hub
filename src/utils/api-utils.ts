b3052f687553c52a157f3ee71bb50507aa2579d4
Krasyo <doanusho@gmail.com>
2024-01-11 09:27:03 +0000

    change server IPs

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from "axios"
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "http://ec2-13-40-213-131.eu-west-2.compute.amazonaws.com:5000/api"
+  baseURL: "http://ec2-18-130-127-9.eu-west-2.compute.amazonaws.com:5000/api"
 })
